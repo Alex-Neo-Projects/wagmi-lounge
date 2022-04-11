882e775e51f588bac1535f39fc0e5d6135cbfffb
reyes <reyesruizalex@gmail.com>
2022-04-10 20:21:57 -0700

    clean up

--- App.js
+++ App.js
@@ -4,29 +4,18 @@ import Animated, {
   useSharedValue,
   useAnimatedStyle,
   useAnimatedGestureHandler,
-  withSpring,
   runOnJS,
 } from 'react-native-reanimated'
 import { PanGestureHandler } from 'react-native-gesture-handler'
 import { useContext, useEffect, useState } from 'react'
-import { socket, SocketContext } from './context/socket'
+import { SocketContext } from './context/socket'
 import { Ball } from './Ball'; 
-
-const data = [
-  {
-    name: 'Cool Cat #1',
-    url:
-      'https://lh3.googleusercontent.com/jJsUhpTNA_9CwMePUgJZamQW1IIHQgt3Hx1of8Y8EHhKqBsjHU9xb03S79xXzqLpGCVUX243N8dxYNkKaRcc51pFQh6bwZg5F8f-Ig',
-  },
-  { name: 'Doodle #1', url: '' },
-]
 
 export default function App() {
   const socket = useContext(SocketContext)
 
   const x = useSharedValue(0)
   const y = useSharedValue(0)
-  const pressed = useSharedValue(false)
 
   const [xValue, setXValue] = useState()
   const [yValue, setYValue] = useState()
@@ -41,20 +30,12 @@ export default function App() {
       height: 100,
       width: 100,
       borderRadius: 100,
-      backgroundColor: pressed.value ? '#FEEF86' : '#001972',
       transform: [{ translateX: x.value }, { translateY: y.value }],
     }
   })
 
-  // two problems: 
-  //  - 1: need to duplicate x amount of 'uasToo'
-  //  - 2: need to have independent socketX, socketY values
-
-  // solution: store socketX, socketY in state array tied to socketId's
-
   const eventHandler = useAnimatedGestureHandler({
     onStart: (event, ctx) => {
-      pressed.value = true
       ctx.startX = x.value
       ctx.startY = y.value
     },
@@ -65,9 +46,6 @@ export default function App() {
       runOnJS(setXValue)(x.value)
       runOnJS(setYValue)(y.value)
     },
-    onEnd: (event, ctx) => {
-      pressed.value = false
-    },
   })
 
   useEffect(() => {
@@ -76,9 +54,8 @@ export default function App() {
     })
   }, [])
 
-  // user1: {x,y}
-  // user2: {x,y}
   useEffect(() => {
+    // update x,y coordinates whenever we receive an emit from the server
     Object.entries(users).map(([key, value]) => {
       if (!(key === socket.id)) {
         let socketXCopy = { ...socketX }; 
@@ -94,6 +71,7 @@ export default function App() {
   }, [users])
 
   useEffect(() => {
+    // emit x,y coordinates of current user to the server
     socket.emit(
       'position',
       JSON.stringify({ socketId: socket.id, x: xValue, y: yValue }),
@@ -112,13 +90,14 @@ export default function App() {
                 width: 100,
                 height: 100,
                 borderRadius: 100,
-                borderColor: 'yellow',
-                borderWidth: 10,
+                borderColor: 'black',
+                borderWidth: 5,
               }}
               source={{ uri: users[socket.id] === undefined ? null : users[socket.id].currImage}}
             />
           </Animated.View>
         </PanGestureHandler>
+        {/* loop thru and show circles for the other users connected to the socket */}
         {Object.entries(users).map(([key, value]) => {
           if (!(key === socket.id)) {
             return(<Ball x={users[key].x} y={users[key].y} imageUrl={users[key].currImage} key={key}/>);
@@ -132,7 +111,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    height: '100%',
+    height: "100%",
     width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![demo](https://user-images.githubusercontent.com/7016669/162658716-54dd4295-2d7b-4f9c-a0b8-e6adf0626452.gif)
 
-
+Note for the demo: this works for n amount of devices (n = number of phones running the app)
 
 name chosen by github
 
