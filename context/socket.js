de7000b47f1562f88fa07eaa169d3e643ae4171e
reyes <reyesruizalex@gmail.com>
2022-04-10 20:04:54 -0700

    switch socket

--- context/socket.js
+++ context/socket.js
@@ -1,6 +1,6 @@
 import socketio from "socket.io-client"
 import { createContext } from 'react'; 
 
-// export const socket = socketio.connect('https://potato-socket-exp.herokuapp.com/');
-export const socket = socketio.connect('http://localhost:3000/');
+export const socket = socketio.connect('https://potato-socket-exp.herokuapp.com/');
+// export const socket = socketio.connect('http://localhost:3000/');
 export const SocketContext = createContext(socket); 
